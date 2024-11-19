87264b17f32d001485a91f0f7c941a06f9bc94cb
Your Name <you@example.com>
2024-11-19 15:50:46 +0330

    Initial Commit

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from requests import *
 
 app = Flask(__name__)
 
+def text_to_text(src_lang, dst_lang, text):
+    url = 'https://api.reverso.net/translate/v1/translation'
+    headers = {
+        'content-type':'application/json',
+        'user-agent': 'Cy'
+    }
+    data = {"format":"text","from":src_lang,"to":dst_lang,"input": text,"options":{"origin":"translation.web"}}
+    try:
+        request = get(url, headers=headers, data=data)
+        return(request.text)
+    except BaseException as e:
+        print(e)
+
+print(text_to_text("eng", "per", "hello world"))
+
+"""
 @app.route("/")
 def handler():
-    return "hello"
+    return text_to_text("eng", "per", "hello stupid asses")
 
 if __name__ == "__main__":
     app.run()
+"""
