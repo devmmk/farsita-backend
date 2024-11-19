b3fc4b443502ea4554674cab4c8d2631b4df1c3a
Your Name <you@example.com>
2024-11-19 16:00:45 +0330

    fix http request method

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def text_to_text(src_lang, dst_lang, text):
     url = 'https://api.reverso.net/translate/v1/translation'
     headers = {
         'content-type':'application/json',
-        'user-agent': 'Cy'
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0'
     }
     data = {"format":"text","from":src_lang,"to":dst_lang,"input": text,"options":{"origin":"translation.web"}}
     try:
-        request = get(url, headers=headers, data=data)
+        request = post(url, headers=headers, json=data)
         return(request.text)
     except BaseException as e:
         print(e)
